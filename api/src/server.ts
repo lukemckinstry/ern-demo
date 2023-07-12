7212f75ad2a33720c25ecd88eb0d0e6f0bd80f64
Luke McKinstry <lukemckinstry@gmail.com>
2023-07-12 10:55:19 -0400

    add unit tests for backend api server

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,6 +1,8 @@
-import { app } from "./app";
+import { get } from "./app";
 
 const port = process.env.PORT || 4000;
+
+const app = get()
 
 app.listen(port, () =>
   console.log(`Server running on port ${port}, http://localhost:${port}`)
